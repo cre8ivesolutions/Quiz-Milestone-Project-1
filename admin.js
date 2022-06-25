450f1edf0fa8d9ae41d142cbdc0d203b39eeea7e
cre8ivesolutions <tammerz78@gmail.com>
2022-06-25 13:35:38 -0500

    first day

---
+++ admin.js
@@ -0,0 +1,22 @@
+// make a start button
+// make a number button for the questions
+//display the question in the question div
+//display the answers in the answers div
+// style the page
+// link the questions and answers
+//add a hover listener for the answers to change the color
+// add a hover listener for the 
+// add the click listener for the answer choice
+//add the logic for increasing/decreasing the counter when the answer is chosen
+// make the counter increase when a correct answer is given 
+// add a timer to the game
+// display the time it took to finish on the last screen
+//randomize the questions
+//add a gif for if they win or lose
+//add a button to restart at the end
+
+//BONUS
+// ask their name at the beginning and add it to the end
+//add  "good job<name>"
+// add a photo to each question
+//add a button to share their score online at the end
