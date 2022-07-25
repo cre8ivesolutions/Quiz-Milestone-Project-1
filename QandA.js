5bbb4fa93b522d69ab9a9f1a9e4de60c9355c3d3
cre8ivesolutions <tammerz78@gmail.com>
2022-07-24 19:57:40 -0500

    updating

--- QandA.js
+++ QandA.js
@@ -2,27 +2,38 @@ let qAndA = [
     {
         questions: "What data type is most useful for holding data in text form?",
         answers: [
-            {text: 'array', correct: 'false',},
-            {text: 'object', correct: 'false'},
-            {text: 'string', correct: 'true'},
-            {text:'const', correct: 'false'},]
+            {a: 'array'},
+            {b: 'object'},
+            {c: 'string'},
+            {d:'const'},]
+            correctAnswer"c";
+        
     },
         
       {questions: "this is the second question?", 
         answers : [
-            {text:'this is the second answer', correct: 'false'},
-            {text: "this is also a second answer", correct: 'false'},
-            {text: 'this is the correct one', correct: 'true'},
-            {text: "this is a second answer",correct: 'false'},]
+            {text:'this is the second answer'},
+            {text: "this is also a second answer"},
+            {text: "this is the correct one"},
+            {text: "this is a second answer"},]
+            correctAnswer: "c";
         },
         
         {questions: "this is the third question?", 
         answers: [
-            {text:'this is the third answer', correct: 'true'},
-            {text: "this is also athirdd answer", correct: 'false'},
-            {text: 'this is also a third answer, the correct one', correct: 'true'},
-            {text: "this is a third answer",correct: 'false'},]
+            {text:'this is the third answer'},
+            {text: "this is also athirdd answer"},
+            {text: 'this is also a third answer'},
+            {text: "this is a third answer"},]
         },    
+        {questions: "this is the correct way to add two classes to an element?", 
+        answers: [
+            {text:"'two, classes'"},
+            {text: "'.two, .classes'"},
+            {text: "'.two classes'"},
+            {text: "an element can only have one class"},]
+            correctAnswer: "c";
+        },
     ]
     
     
--- main.js
+++ main.js
@@ -6,7 +6,7 @@ startOnClick.addEventListener('click', startGame=()=>{
     let showMainContainer = document.querySelector('main');
     let hideStartButton = document.getElementById('startButton');
     let hideStartScreen = document.getElementById('start-screen');
-    let addQuestion = document.getElementById('question');
+    let addQuestion = document.getElementById('questionContainer');
     
     showMainContainer.classList.remove('hide');
     hideStartButton.classList.add('hide');
@@ -16,7 +16,7 @@ startOnClick.addEventListener('click', startGame=()=>{
 
 function addQuestionText(){
     let nextQuestion = document.getElementById('question');
-    let questionText = question.innerText;
+    let questionText = ;
 
     for (let i = 0; i < qAndA.length; i++) {
         console.log(qAndA[i]);
