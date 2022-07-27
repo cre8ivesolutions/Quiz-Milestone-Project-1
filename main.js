0edc352d9538b97132f90411a4d1700aa561ebd7
cre8ivesolutions <tammerz78@gmail.com>
2022-07-27 13:52:40 -0500

    save

--- main.js
+++ main.js
@@ -1,58 +1,67 @@
 let qAndA = [
     {
-        questions: "What data type is most useful for holding data in text form?",
+        q: "What data type is most useful for holding data in text form?",
         answers: [
             {a: 'array', type: 'incorrect'},
-            {a: 'object', type: 'incorrect'},
-            {a: 'string', type: 'correct'},
-            {a:'const', type: 'incorrect'},]        
+            {b: 'object', type: 'incorrect'},
+            {c: 'string', type: 'correct'},
+            {d:'const', type: 'incorrect'},]        
     },
         
-      {questions: "this is the second question?", 
+      {q: "this is the second question?", 
         answers : [
             {a:'this is the second answer', type: 'incorrect'},
-            {a: "this is also a second answer", type: 'incorrect'},
-            {a: "this is the correct one", type: 'correct'},
-            {a: "this is a second answer", type: 'incorrect'},]
+            {b: "this is also a second answer", type: 'incorrect'},
+            {c: "this is the correct one", type: 'correct'},
+            {d: "this is a second answer", type: 'incorrect'},]
         },
         
-        {questions: "this is the third question?", 
+        {q: "this is the third question?", 
         answers: [
             {a:'this is the third answer', type: 'incorrect'},
-            {a: "this is the correct third answer", type: 'correct'},
-            {a: 'this is also a third answer', type: 'incorrect'},
-            {a: "this is a third answer", type: 'incorrect'},]
+            {b: "this is the correct third answer", type: 'correct'},
+            {c: 'this is also a third answer', type: 'incorrect'},
+            {d: "this is a third answer", type: 'incorrect'},]
         },    
-        {questions: "What is the correct way to add two classes to an element?", 
+        {q: "What is the correct way to add two classes to an element?", 
         answers: [
             {a:"'two, classes'", type: 'incorrect'},
-            {a: "'.two, .classes'", type: 'incorrect'},
-            {a: "'.two classes'", type: 'correct'},
-            {a: "an element can only have one class", type: 'incorrect'},]
+            {b: "'.two, .classes'", type: 'incorrect'},
+            {c: "'.two classes'", type: 'correct'},
+            {d: "an element can only have one class", type: 'incorrect'},]
         },
     ]
+    
+    
+    
+    
     let startOnClick = document.getElementById('startButton');
     let clickOnAnswer = document.getElementsByClassName('answer');
     let encourage = document.getElementById('encouragement');
     
-startOnClick.addEventListener('click', startGame=()=>{
-    let showMainContainer = document.querySelector('main');
-    let hideStartButton = document.getElementById('startButton');
-    let hideQuizImage = document.getElementById('quizTime');
-
-    showMainContainer.classList.remove('hide');
-    hideStartButton.classList.add('hide');
-    hideQuizImage.classList.add('hide');
-    encourage.classList.add('hide');
-
-    function addQuestionText(){
-        // let qText   
-        let questionElement = document.createElement('h2');
-        const questionText = document.createTextNode("questions from array" )
-
-        questionElement.appendChild(questionText);
-        document.getElementById('questionContainer').appendChild(questionElement);
-        //the next couple of lines are not working correctly to display the quetions in the array of qAndA.questions 
+    startOnClick.addEventListener('click', startGame=()=>{
+        let showMainContainer = document.querySelector('main');
+        let hideStartButton = document.getElementById('startButton');
+        let hideQuizImage = document.getElementById('quizTime');
+        
+        showMainContainer.classList.remove('hide');
+        hideStartButton.classList.add('hide');
+        hideQuizImage.classList.add('hide');
+        encourage.classList.add('hide');
+        
+        function addQuestionText(){
+            // let qText   
+            let questionElement = document.createElement('h2');
+            let qText = questionText;
+            let questionText = document.createTextNode("questions from array" )
+            
+            questionElement.appendChild(questionText);
+            document.getElementById('questionContainer').appendChild(questionElement);
+            Array.from(qAndA.q).forEach(q=> {
+                document.createTextNode = qText
+            })
+            //7/27 2 pm
+            //the next couple of lines are not working correctly to display the quetions in the array of qAndA.questions 
     
     
         let text = "";
